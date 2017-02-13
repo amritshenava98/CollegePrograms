6e6fc9ddaef9c9c2498e82102835431858612333
Amrit Shenava <amritshenava98@users.noreply.github.com>
2017-02-13 15:45:22 +0400

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+ int a, b;
+ clrscr();
+ cout<<"\n Enter a value for a : ";
+ cin>>a;
+ cout<<"\n Enter a value for b : ";
+ cin>>b;
+ cout<<"\n The sum of "<<a<<" and "<<b<<" is "<<a+b;
+ cout<<"\n The product of "<<a<<" and "<<b<<" is "<<a*b;
+ cout<<"\n The difference between "<<a<<" and "<<b<<" is "<<a-b;
+ cout<<"\n The division product of "<<a<<" and "<<b<<" is "<<a/b;
+ cout<<"\n The percentage between "<<a<<" and "<<b<<" is "<<(a/b)*100<<"%";
+ cout<<"\n The increment of a "<<a<<" is "<<a++<<" and "<<b<<" is "<<b++;
+ cout<<"\n The decrement of a "<<a<<" is "<<a--<<" and "<<b<<" is "<<b--;
+ getch();
+}
